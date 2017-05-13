6fe35ff781e6db089104790907523b2fe8ef089c
Arpit Khandelwal <arpit.khandelwal@rail.bombardier.com>
2017-05-13 10:27:26 +0530

    first commit

---
+++ RPNCalcTest/RPNCalc.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPNCalcTest
+{
+    public class RPNCalc
+    {
+    private int numinput;
+
+    public int AddTwoNumber(string s0 )
+    {
+      Stack<int> stackone = new Stack<int>();
+     // string s0 = Console.ReadLine();
+      for (int i = 0; i < s0.Length; i++)
+      {
+        if (!s0[i].Equals("+") && !s0[i].Equals("-") && !s0[i].Equals("*") && !s0[i].Equals("%"))
+        {        
+            numinput = Convert.ToInt32(s0[i]);
+            stackone.Push(numinput);
+        }
+        else if(s0[i].Equals("+"))
+        {
+          int store1 = stackone.Pop();
+          int store2 = stackone.Pop();
+          stackone.Push(store2 + store1);
+        }
+        else if (s0[i].Equals("-"))
+        { 
+            int store1 = stackone.Pop();
+            int store2 = stackone.Pop();
+            stackone.Push(store2 - store1);
+         }
+        else if (s0[i].Equals("*"))
+        {
+          int store1 = stackone.Pop();
+          int store2 = stackone.Pop();
+          stackone.Push(store2 * store1);
+        }
+        else if (s0[i].Equals("/"))
+        {
+          int store1 = stackone.Pop();
+          int store2 = stackone.Pop();
+          stackone.Push(store2 / store1);
+        }
+      }
+      return stackone.Pop();
+    }
+    static void Main(string[] args)
+    {
+      RPNCalcexp obj = new RPNCalcexp();
+      obj.TestCase1();     
+    }
+    }
+}
---
+++ RPNCalcTest/RPNCalcexp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace RPNCalcTest
+{
+  [TestFixture]
+  class RPNCalcexp
+  {
+    [Test]
+    public void TestCase1()
+      {
+      RPNCalc RPNCalc = new RPNCalc();
+      Assert.AreEqual(4 , RPNCalc.AddTwoNumber("2,2,+"));
+      }
+ 
+    //public void TestCase2()
+    //{
+
+    //}
+  }
+}
